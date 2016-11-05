31a596b5533669ad95ac7e0a835664c30124e1fa
samda <muy.samda13@gmail.com>
2016-11-05 17:02:58 +0700

    fix conflig

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -1,11 +1,7 @@
 defmodule Metathor.UserSocket do
   use Phoenix.Socket
 
-  ## Channels
-  # channel "room:*", Metathor.RoomChannel
-
-  ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
@@ -34,7 +30,4 @@ defmodule Metathor.UserSocket do
   #
   # Returning `nil` makes this socket anonymous.
   def id(_socket), do: nil
-
-  ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
 end
